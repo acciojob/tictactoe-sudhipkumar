4c295f5676b57cacf558f3c4a158c99b7ad5a210
sudhip kumar <123609652+sudhipkumar@users.noreply.github.com>
2025-04-01 20:10:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+ document.getElementById("submit").addEventListener("click", function() {
+            const player1 = document.getElementById("player-1").value;
+            const player2 = document.getElementById("player-2").value;
+            if (player1 && player2) {
+                document.getElementById("board").style.display = "grid";
+                document.querySelector(".message").innerText = `${player1}, you're up!`;
+                startGame(player1, player2);
+            }
+        });
