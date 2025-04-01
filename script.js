c837ca1046b6d41474720967164b5c44bf4b7f83
sudhip kumar <123609652+sudhipkumar@users.noreply.github.com>
2025-04-01 20:10:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -33,3 +33,12 @@ function startGame(player1, player2) {
                 }, { once: true });
             });
         }
+
+function checkWinner(board, symbol) {
+            const winPatterns = [
+                [0, 1, 2], [3, 4, 5], [6, 7, 8],
+                [0, 3, 6], [1, 4, 7], [2, 5, 8],
+                [0, 4, 8], [2, 4, 6]
+            ];
+            return winPatterns.some(pattern => pattern.every(index => board[index] === symbol));
+        }
